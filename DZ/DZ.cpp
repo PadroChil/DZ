94648ce4e9f9e6ce99780cfd73d9c2d605352e04
Evgen <Evgen@DESKTOP-FB8ANK3>
2025-10-03 10:43:07 +0300

    567

--- DZ/DZ.cpp
+++ DZ/DZ.cpp
@@ -20,7 +20,7 @@ int main()
 	{
 		std::cout << "\nВведите шестизначное число  ";
 		std::cin >> num;
-		if (num < 100000)
+		if (num >= 100000 && num <= 999999)
 		{
 			break;
 		}
@@ -32,63 +32,72 @@ int main()
 
 		if (sumwer = sumtyr)
 		{
-			std::cout << "Счаствилое число :)";
+			std::cout << "\nСчаствилое число :)";
 		}
 		else
 		{
-			std::cout << "Не повезло :(";
+			std::cout << "\nНе повезло :(";
 		}
-		
-
 	}*/
 
+	
+
 	//Задача 2
-	/*std::cout << "\nЗадача 2";
+	std::cout << "\nЗадача 2";
 
-	int num;
+	int number;
+
+    std::cout << "\nВведите четырехзначное число: ";
+    std::cin >> number;
+
+    if ( number >= 1000 && number <= 9999 || number <= -1000 && number >= -9999) 
+	{
+ 
+        int Number = number > 0 ? number : -number;
+
+        int dig1 = Number / 1000;            
+        int dig2 = (Number / 100) % 10;      
+        int dig3 = (Number / 10) % 10;       
+        int dig4 = Number % 10;               
+
+        int newNumber = dig2 * 1000 + dig1 * 100 + dig4 * 10 + dig3;
+
+        
+        if (number < 0) 
+		{
+            newNumber = -newNumber;
+        }
+
+        std::cout << "\nРезультат: " << newNumber;
+    } 
+	else 
+	{
+		std::cout << "\nОшибка: введено не четырехзначное число.";
+	}
 
 
 
-
-
-
-
-
-
-
-
-
-	*/
+	
 	// Задача 3
 	std::cout << "\nЗадача 3";
 	
-	int max;
-	int num;
-	int count = 0;
-	std::cout << "\nВведите 7 чисел ";
-	
-	for (int i = 0; i < 7; i++)
-	{
-		std::cin >> num;
-		if (num < 0)
-		{
-			break;
-		}
-		if (count == 0)
-		{
-			max == count;
-		}
-		else
-		{
-			if (num > max)
-			{
-				max == num;
-			}
-		}
+	int max; 
+	int numm; 
 
+	std::cout << "\nВведите 7 целых чисел: ";
+	std::cin >> numm;
+	max = numm;
+
+	for (int i = 1; i < 7; ++i) {
+		std::cin >> numm;
+
+		if (numm > max) 
+		{
+			max = numm;
+		}
 	}
-	std::cout << "Максимальное число " << max;
-	
+
+	std::cout << "\nСамое большое число: " << max;
 
 
 
